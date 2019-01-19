24a51bc6842f61b34482f770305387bb7e3f0df1
MontoyaHimself <men2510@email.vccs.edu>
2019-01-19 17:04:57 -0500

    remainder method

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,4 +15,8 @@ public class Calculator {
     public static double divide(double num1, double num2){
         return num1 / num2;
     }
+
+    public static double remainder (double num1, double num2){
+        return num1 % num2;
+    }
 }
